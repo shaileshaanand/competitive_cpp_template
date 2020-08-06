6192d2a7dcfb466a722e5dbdea43ef35abf6e858
shaileshaanand <anaandshailu@gmail.com>
2020-08-06 11:47:31 +0530

    initial commit

---
+++ template.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define fo(i, n) for (i = 0; i < n; i++)
+#define PI 3.1415926535897932384626
+
+// using namespace std;
+
+template <typename T>
+void readarray(T *arr, size_t l)
+{
+    int32_t i;
+    fo(i, l)
+    {
+        std::cin >> arr[i];
+    }
+}
+
+template <typename T>
+void printarray(T *arr, size_t l, std::string sep = " ")
+{
+    int32_t i;
+    fo(i, l)
+    {
+        if (i == l - 1)
+        {
+            std::cout << arr[i];
+        }
+        else
+        {
+            std::cout << arr[i] << sep;
+        }
+    }
+    std::cout << std::endl;
+}
+
+std::string solve()
+{
+    return "0";
+}
+
+int main()
+{
+    int32_t n_test_cases = 1;
+    // cin >> n_test_cases;
+    while (n_test_cases--)
+    {
+        int32_t i, j, k, a, b, c, l, m, n;
+    }
+    return 0;
+}
